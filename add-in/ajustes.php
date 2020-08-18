a01587abefe94784ff9de6d898d4bd20d345bd1c
fixonweb <contato@fixonweb.com.br>
2020-08-18 17:19:10 -0300

    0.1.10

--- add-in/ajustes.php
+++ add-in/ajustes.php
@@ -28,7 +28,7 @@ function fix159775_add_to_cart($atts, $content = null){
 
 
 	<div class="elementor-button-wrapper">
-		<a href="#" data-id="<?php echo $post->ID ?>" class="fix159775_add_to_cart<?php echo $post->ID ?> elementor-button-link elementor-button elementor-size-sm" role="button">
+		<a href="#" data-id="<?php echo $post->ID ?>" class="fix159775_add_to_cart_<?php echo $post->ID ?> elementor-button-link elementor-button elementor-size-sm" role="button">
 			<span class="elementor-button-content-wrapper">
 				<span class="elementor-button-text">ADICIONAR AO ORÇAMENTO</span>
 			</span>
