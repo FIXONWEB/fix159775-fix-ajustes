559727fd6fc63a000727a27bac5bea1680825961
fixonweb <contato@fixonweb.com.br>
2020-08-18 09:20:33 -0300

    0.1.2

--- add-in/ajustes.php
+++ add-in/ajustes.php
@@ -1,6 +1,6 @@
 <?php
-add_shortcode("fix158716_deletar", "fix158716_deletar");
-function fix158716_deletar($atts, $content = null){
+add_shortcode("fix159775_add_to_cart", "fix159775_add_to_cart");
+function fix159775_add_to_cart($atts, $content = null){
 	?>
 	<script type="text/javascript">
 		jQuery(function($){
